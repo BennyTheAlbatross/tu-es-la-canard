5c6949c54e03476f7aafc3a1c18c4fa0e857fd79
Benny-Bean <benedict@sbiomedic.com>
2026-05-04 19:05:48 +0200

    added textuers and enemy1

--- Duck_movments.py
+++ Duck_movments.py
@@ -11,34 +11,63 @@
 
 3. Duck class
    - stores image, rect, spee
-   - has a move(direction) method
-'''
+   - has a move(direction) method '''
 
 import pygame
 # Constants
 ASSET_FOLDER = 'assets'
 DUCK_SPEED = 5
 DUCK_SIZE = (50, 50)
+ENEMY_SIZE = (70, 70)
 
 def load_duck_images():
-    duck_frunt = pygame.image.load(f'{ASSET_FOLDER}/duck_front.png')
-    duck_back = pygame.image.load(f'{ASSET_FOLDER}/duck_back.png')
-    duck_right = pygame.image.load(f'{ASSET_FOLDER}/duck_walk1.png')
-    duck_left = pygame.image.load(f'{ASSET_FOLDER}/duck_walk2.png')# this need to be inverted
+    duck_front= pygame.image.load(f'{ASSET_FOLDER}\duck_front.png')
+    duck_back = pygame.image.load(f'{ASSET_FOLDER}\duck_back.png')
+    duck_right = pygame.image.load(f'{ASSET_FOLDER}\duck_walk_1.png')
+    duck_left = pygame.image.load(f'{ASSET_FOLDER}\duck_walk_2.png')# this need to be inverted
     duck_left = pygame.transform.flip(duck_left, True, False)  # Flip the image horizontally
+
+    #scale the images so they can be defined with DUCK_SIZE
+    duck_front = pygame.transform.scale(duck_front, DUCK_SIZE)
+    duck_back = pygame.transform.scale(duck_back, DUCK_SIZE)
+    duck_right = pygame.transform.scale(duck_right, DUCK_SIZE)
+    duck_left = pygame.transform.scale(duck_left, DUCK_SIZE)
+
     return {
-        'front': duck_frunt,
+        'front': duck_front,
         'back': duck_back,
         'right': duck_right,
         'left': duck_left
     }
+def load_enemy_images():
+    enemy_fire = pygame.image.load(f'{ASSET_FOLDER}\enemy_fire.png')
+    enemy_water = pygame.image.load(f'{ASSET_FOLDER}\enemy_water.png')
+    enemy_rock = pygame.image.load(f'{ASSET_FOLDER}\enemy_rock.png')
 
-class Duck:
-    def __init__(self, x, y)
+    ennemy_fire = pygame.transform.scale(enemy_fire, ENEMY_SIZE)
+    return {
+        'enemy_fire': enemy_fire,
+        'enemy_water': enemy_water,
+        'enemy_rock': enemy_rock,
+    }
+
+class duck:
+    def __init__(self, x, y):
         self.images = load_duck_images()
         self.image = self.images['front']
-        self.rect = self.image.get_rect(topleft=(x, y))
+        self.rect = self.image.get_rect(center=(x, y)) # this ancords the image. 
         self.speed = DUCK_SPEED
+
+#define user input for duck movement
+    def handle_input(self, keys):
+        if keys[pygame.K_UP]:
+            self.move('up')
+        elif keys[pygame.K_DOWN]:
+            self.move('down')
+        elif keys[pygame.K_RIGHT]:
+            self.move('right')
+        elif keys[pygame.K_LEFT]:
+            self.move('left')
 
     def move(self, direction):
         if direction == 'up':
@@ -53,3 +82,21 @@ class Duck:
         elif direction == 'left':
             self.rect.x -= self.speed
             self.image = self.images['left']
+
+
+#define class enemy  just use a square as a place holder. 
+class Enemy:
+    def __init__(self, x, y):
+        self.images = load_enemy_images()# Load the enemy image using the load_enemy_images function.
+        self.image = self.images['enemy_fire']
+        self.rect = self.image.get_rect(center =(x, y))
+        self.speed = 3
+        self.type = 'fire'
+        self.direction = 1  # 1 for right, -1 for left
+    
+    def move(self):
+        self.rect.x += self.speed  * self.direction # Move the enemy to the right
+        if self.rect.left > 800:  # If the enemy goes off the screen, reset its position self.direction = -1  # Change direction to left if self.rect.right < 0:  # If the enemy goes off the screen, reset its position
+            self.direction = -1  # Change direction to right
+        if self.rect.right < 0:
+            self.direction = 1  # Change direction to left
--- tu_es_la_canard.py
+++ tu_es_la_canard.py
@@ -8,31 +8,61 @@ import pygame
 import sys
 import os
 
-from Duck_movments import load_duck_images, Duck 
+from Duck_movments import duck, Enemy
 
 # define current dir to link to assests and utilities. use same dir as the script for now.
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 def main():
-   # Initialise the screen 
+    # srart py game
     pygame.init()
+   # Initialise the screen 
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Tu es le canard")
     
-    # fill the background dark blue, add a boarder or dark purple. 
+#import textuers here and scale them to the correct size for the game.
+    background_image = pygame.image.load(os.path.join(current_dir, 'assets', 'tile_dirt.png')).convert()
+    border_image = pygame.image.load(os.path.join(current_dir, 'assets', 'tile_cave.png')).convert()
+#now scale the images to the correct size for the game.
+    background_image = pygame.transform.scale(background_image, (50, 50)) # this will scale
+    border_image = pygame.transform.scale(border_image, (50, 50)) # this will scale the border image to the correct size for the game.
+
+
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((0, 0, 50)) # dark blue background
-    boarder_color = (50, 0, 50) # dark purple color for the border
-    pygame.draw.rect(background, boarder_color, background.get_rect(), 50) # draw the border with a thickness of 5 pixels
+    border_image = pygame.image.load(os.path.join(current_dir, 'assets', 'tile_cave.png')).convert()
+
+    #draw the image across the entire background, tile it to fill the screen.
+    tile_width = background_image.get_width()
+    tile_height = background_image.get_height()
+
+    for x in range(0,800, tile_width):
+        for y in range(0,600, tile_height):
+            background.blit(background_image, (x, y)) # draw the background image across the entire screen.
+
+    border_thickness = 40
+    cave_width = border_image.get_width()
+    cave_height = border_image.get_height()
+
+    for x in range(0, 800, cave_width):
+        for y in range(0, border_thickness, cave_height):
+            background.blit(border_image, (x, y)) # draw the top border
+            background.blit(border_image, (x, 600 - border_thickness + y)) # draw the bottom border
+
+    for y in range(0, 600, cave_height):
+        for x in range(0, border_thickness, cave_width):
+            background.blit(border_image, (x, y)) # draw the left border
+            background.blit(border_image, (800 - border_thickness + x, y)) # draw the right border
 
 
-    # load the duck sprite, use the front image for now, we can add more later.
-    duck_images = load_duck_images(f"{current_dir}/assets")
-    duck_image, duck_rect = create_duck(duck_images, center=(400, 300))
 
-    # main game loop
+    # start clock
     clock = pygame.time.Clock()
+    # create one duck at the center of the screen.
+    player_duck = duck( 400, 300)
+    #create the an enemy
+    enemy1 = Enemy(100, 100) # this will create an enemy at the top left corner of the screen.
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -41,13 +71,14 @@ def main():
         
         # move the duck with arrow keys and change the sprite to the correct direction.
         keys = pygame.key.get_pressed()
-        duck_image = move_duck(duck_rect, keys, duck_images, duck_image)
-        
+        player_duck.handle_input(keys) # this function should be defined in the duck class
+        enemy1.move() # this function should be defined in the enemy class to move the enemy across the screen.
         # draw everything
-        screen.blit(background, (0, 0))
-        screen.blit(duck_image, duck_rect)
+        screen.blit(background, (0, 0)) 
+        screen.blit(player_duck.image, player_duck.rect) # this assumes the duck class has an image and rect attribute for drawing
+        screen.blit(enemy1.image, enemy1.rect) # this assumes the enemy class has an image and rect attribute for drawing
         pygame.display.flip()
-        
         clock.tick(60)  # limit to 60 frames per second
-if __name__ == "__main__":
+
+if __name__ == "__main__": # this is the standard way to run the main function in Python, it checks if the script is being run directly (as the main program) and not imported as a module in another script. If this condition is true, it calls the main() function to start the game. 
     main()
